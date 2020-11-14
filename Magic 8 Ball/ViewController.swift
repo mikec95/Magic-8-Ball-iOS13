522813e7974963b9bf91347e44274c5103d45e58
Mike Carbonari <mikecarbonari1@gmail.com>
2020-11-14 13:01:50 -0600

    Add outlet variables and action for press of ask button. Set image based on random index of 8 ball array of images when ask button is pressed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    // Reference variable to 8 ball image
+    @IBOutlet weak var eightBall: UIImageView!
 
+    // Create array of 8 balls for display
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        // Set 8 ball image to random output
+        eightBall.image = ballArray[Int.random(in: 0...4)]
+    }
 }
-
